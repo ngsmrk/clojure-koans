166378811f00e0d40c2190864d82d3ff6e4950b7
Angus Mark <ngsmrk@gmail.com>
2014-12-25 13:47:44 +0000

    Solved but need to revisit

--- src/koans/14_destructuring.clj
+++ src/koans/14_destructuring.clj
@@ -25,23 +25,34 @@
   (= "Rich Hickey aka The Clojurer aka Go Time aka Macro Killah"
      (let [[first-name last-name & aliases]
            (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
-       __))
+            (let [[first-name last-name & aliases]
+                  (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
+                  (apply str(interpose " " (conj (interleave (repeat "aka") aliases) last-name first-name)))
+              )
+       ))
 
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-       __))
+       {:original-parts full-name
+       :named-parts {:first first-name :last last-name}}
+     )
+  )
 
   "Break up maps by key"
   (= "123 Test Lane, Testerville, TX"
      (let [{street-address :street-address, city :city, state :state} test-address]
-       __))
+       (str street-address ", " city ", " state)))
 
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address __ __]} test-address]
-       __))
+     (let [{:keys [street-address city state]} test-address]
+       (str street-address ", " city ", " state)))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (___ ["Test" "Testerson"] test-address)))
+     ((fn [[fname lname]
+                                    {:keys [street-address city state]}]
+                                  (str fname " " lname ", "
+                                       street-address ", " city ", " state))
+                                 ["Test" "Testerson"] test-address)))
